fb53826c7428b0e1c1a6c86d05d6ee29e9021337
KeeganWinter125 <107592551+KeeganWinter125@users.noreply.github.com>
2023-06-20 00:38:11 -0400

    New Small Image

--- app/Menu.js
+++ app/Menu.js
@@ -9,33 +9,33 @@ export default function Menu() {
   const [mainMenu, setMainMenu] = useState(false);
 
   return (
-    <motion.div className="absolute bg-gradient-to-r from-black border-t border-r border-neutral-800 h-full mt-12 w-32 z-20">
+    <motion.div className="absolute bg-gradient-to-r from-black border-t border-r border-neutral-600 h-full mt-12 w-32 z-20">
       <ul>
-        <li className="flex text-neutral-500 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r hover:text-neutral-200 w-32  text-sm">
+        <li className="flex text-neutral-400 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r-2 hover:text-neutral-200 w-32  text-sm">
           <Link href="#" className="py-3 px-3">
             Home
           </Link>
         </li>
 
-        <li className="text-center w-futext border-b border-neutral-800 text-emerald-700 py-3">
+        <li className="w-full border-b border-neutral-600 text-emerald-700 py-3 px-3">
           Creatures
         </li>
-        <li className="flex text-neutral-500 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r hover:text-neutral-200 w-32 text-sm">
+        <li className="flex text-neutral-400 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r-2 hover:text-neutral-200 w-32 text-sm">
           <Link href="#" className="py-3 px-3">
             Dragons
           </Link>
         </li>
-        <li className="flex text-neutral-500 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r hover:text-neutral-200 w-32 text-sm">
+        <li className="flex text-neutral-400 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r-2 hover:text-neutral-200 w-32 text-sm">
           <Link href="#" className="py-3 px-3">
             Beasts
           </Link>
         </li>
-        <li className="flex text-neutral-500 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r hover:text-neutral-200 w-32 text-sm">
+        <li className="flex text-neutral-400 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r-2 hover:text-neutral-200 w-32 text-sm">
           <Link href="#" className="py-3 px-3">
             Spirits
           </Link>
         </li>
-        <li className="flex text-neutral-500 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r hover:text-neutral-200 w-32 text-sm">
+        <li className="flex text-neutral-400 items-center hover:bg-neutral-900  hover:border-r-emerald-400 hover:border-r-2 hover:text-neutral-200 w-32 text-sm">
           <Link href="#" className="py-3 px-3">
             Fair Folk
           </Link>
